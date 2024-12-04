751e65d593649ef19f818e5427ee7834b4b19c34
Miguel-Angel-py <52719247+Miguel-Angel-py@users.noreply.github.com>
2024-12-04 13:34:49 +0100

    Terminadas las funcionalidades de los elementos. Queda hacer el testeo de todas las funcionalidades, pero previamente conectar y formar la BBDD.

--- src/main/java/com/miguel/GestionListas/Controlador/ElementoControlador.java
+++ src/main/java/com/miguel/GestionListas/Controlador/ElementoControlador.java
@@ -3,9 +3,9 @@ package com.miguel.GestionListas.Controlador;
 import com.miguel.GestionListas.Modelo.ElementoModelo;
 import com.miguel.GestionListas.Servicios.ElementoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +17,27 @@ public class ElementoControlador {
     private ElementoServicio elementoServicio;
 
     @GetMapping
-    public List<ElementoModelo> getElementos(){
-        return elementoServicio.getElementos();
+    public List<ElementoModelo> devolverElementos(){
+        return elementoServicio.devolverElementos();
+    }
+
+    @GetMapping("/{Id}")
+    public ElementoModelo devolverElementoById(@PathVariable Integer Id){
+        return elementoServicio.devolverElementoPorId(Id);
+    }
+
+    @PostMapping
+    public ResponseEntity<HttpStatus> crearElemento(@RequestBody ElementoModelo elemento){
+        return elementoServicio.crearElemento(elemento);
+    }
+
+    @DeleteMapping("/{Id}")
+    public ResponseEntity<HttpStatus> borrarElemento(@PathVariable Integer Id){
+        return elementoServicio.borrarElemento(Id);
+    }
+
+    @PutMapping("/{Id}")
+    public ResponseEntity<HttpStatus> actualizarElemento(@PathVariable Integer Id, @RequestBody ElementoModelo elemento){
+        return elementoServicio.actualizarElemento(Id, elemento);
     }
 }
--- src/main/java/com/miguel/GestionListas/Servicios/ElementoServicio.java
+++ src/main/java/com/miguel/GestionListas/Servicios/ElementoServicio.java
@@ -5,6 +5,8 @@ import com.miguel.GestionListas.Repositorios.ElementoRepositorio;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,7 +18,60 @@ public class ElementoServicio {
     @Autowired
     private ElementoRepositorio elementoRepositorio;
 
-    public List<ElementoModelo> getElementos() {
+    public List<ElementoModelo> devolverElementos() {
         return elementoRepositorio.findAll();
+    }
+
+    public ElementoModelo devolverElementoPorId(Integer Id) {
+        try {
+
+            return elementoRepositorio.findById(Id).get();
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public ResponseEntity<HttpStatus> crearElemento(ElementoModelo elemento) {
+        try {
+
+            elementoRepositorio.save(elemento);
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public ResponseEntity<HttpStatus> borrarElemento(Integer Id) {
+        try {
+
+             elementoRepositorio.deleteById(Id);
+             return new ResponseEntity<>(HttpStatus.OK);
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        }
+    }
+
+    public ResponseEntity<HttpStatus> actualizarElemento(Integer Id, ElementoModelo elemento) {
+        try {
+
+            if(elementoRepositorio.existsById(Id)) {
+                elementoRepositorio.deleteById(Id);
+                elementoRepositorio.save(elemento);
+                return new ResponseEntity<>(HttpStatus.OK);
+            }else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
